2ef171b4b70058b741a560c6515e1911a54bad30
fef339 <fitrah.firdaus@gmail.com>
2023-12-25 12:53:36 +0700

    Adding Test For Delete Key

--- test/integration_test.go
+++ test/integration_test.go
@@ -63,7 +63,7 @@ func TestSuite_Run(t *testing.T) {
 	suite.Run(t, new(TestSuite))
 }
 
-func (s *TestSuite) TestCreateOrUpdateKey() {
+func (s *TestSuite) Test_001_CreateOrUpdateKey() {
 
 	req := httptest.NewRequest("POST", "/api/kv", strings.NewReader(`{"key":"test","value":"test value"}`))
 	req.Header.Set("Content-Type", "application/json")
@@ -73,7 +73,7 @@ func (s *TestSuite) TestCreateOrUpdateKey() {
 	s.Equal(http.StatusOK, resp.StatusCode)
 }
 
-func (s *TestSuite) TestGetKey() {
+func (s *TestSuite) Test_002_GetKey() {
 
 	req := httptest.NewRequest("GET", "/api/kv/test", nil)
 	req.Header.Set("Accept", "application/json")
@@ -81,4 +81,19 @@ func (s *TestSuite) TestGetKey() {
 	resp, err := app.Test(req)
 	s.NoError(err)
 	s.Equal(http.StatusOK, resp.StatusCode)
+}
+
+func (s *TestSuite) Test_003_DeleteKey() {
+	req := httptest.NewRequest("DELETE", "/api/kv/test", nil)
+	req.Header.Set("Accept", "application/json")
+
+	resp, err := app.Test(req)
+	s.NoError(err)
+	s.Equal(http.StatusOK, resp.StatusCode)
+
+	req = httptest.NewRequest("GET", "/api/kv/test", nil)
+	req.Header.Set("Accept", "application/json")
+
+	resp, err = app.Test(req)
+	s.Equal(http.StatusNotFound, resp.StatusCode)
 }
